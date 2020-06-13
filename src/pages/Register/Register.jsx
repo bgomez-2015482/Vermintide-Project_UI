4e92ec3445b6557a963cd323abbd6bf16cc55764
Bgomez-2015482 <bgomez-2015482@kinal.edu.gt>
2020-06-12 18:00:55 -0600

    New estructured work

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,15 +1,50 @@
-import React, {useState} from "react";
+import React from "react";
 import "./Register.css";
 
 const Register = () => {
-    const [counter, setCounter] = useState (0)
-
   return (
     <div>
-      <h1>Register</h1>
-      <h2>Angel gay</h2>{" "}
-    <h3>{counter}</h3>
-    <button onClick = {()=>{setCounter(counter+1)}}>Increment</button>
+        <body>
+          <header>
+          <div class="container-header">
+            <div class="header-container container-logo">
+            <img src="../img/oneL2.png" alt="logo" class="img-logo"></img>
+            </div>
+            <div class="header-container container-searchbar">
+              <form action= "" method="post">
+                <div class="input-searchbar">
+                  <input type="search" class="input-addon input-search" id="search" name="search" placeholder="Search" autoComplete="off" required/>
+                </div>
+                <button type="submit" class="submit-searchbar">
+                  <i class="material-icons">search</i>
+                </button>
+              </form>
+            </div>
+            <div class="header-container container-access">
+              <button class="access-button" onClick="location='#'">Log in</button>
+            </div>
+          </div>
+          </header>
+          <div id="container-register">
+            <div id="title">
+              <i class="material-icons lock"></i> Register
+            </div>
+            <form>
+              <div class="input">
+                <div class="input-addon">
+                  <i class="material-icons">email</i>
+                </div>
+                <input id="email" placeholder="Email" type="email" required class="validate" autoComplete="off"/>
+              </div>
+              <div class="clearfix"></div>
+              <div class="input">
+                <div class="input-addon">
+                  <i class ="material-icons">face</i>
+                </div>
+              </div>
+            </form>
+          </div>
+        </body>
     </div>
   );
 };
